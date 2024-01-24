88700b2706740da967b3669f0d31ff1c9376647f
mahesh <maheshghamand@gmail.com>
2024-01-24 14:25:24 +0530

    backend boilerplate

---
+++ backend/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Register struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Post("/register", func(c *fiber.Ctx) error {
+		p1 := new(Register)
+		if err := c.BodyParser(p1); err != nil {
+			return c.Status(http.StatusNotAcceptable).JSON(fiber.Map{
+				"message": "Cannot parse JSON",
+			})
+		}
+		return c.JSON(p1)
+	})
+
+	app.Listen(":3001")
+}
